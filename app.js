d5b6e4d4192655de671e64d2ee570dcf5f622d69
00011776 <wiut00011776@gmail.com>
2021-12-07 18:21:41 +0500

    i add some js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const mobileBurger = document.querySelector(".mobile__burger");
+const mobileNav = document.querySelector(".mobile__nav");
 
+mobileBurger.addEventListener("click", () => {
+  mobileBurger.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+});
+
+const offers = document.querySelectorAll(".offer-filter");
+
+offers.forEach((offer) => {
+  offer.addEventListener("click", () => {
+    offer.classList.toggle("offer-active");
+  });
+});
